4674e36b0527da22d8fce44d6c79b1ba9255d217
Prem <premkmishra.info@gmail.com>
2017-03-10 18:00:36 +0530

    modified linked list implementation

--- LinkedList/src/main/java/com/learnjavabyexample/core/custom/linkedlist/Client.java
+++ LinkedList/src/main/java/com/learnjavabyexample/core/custom/linkedlist/Client.java
@@ -13,22 +13,129 @@ public class Client {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+//		testBasicFunctionOnLinkedList();
+//		retainMNodeThenDeleteMNodeTest();
+		testCircularity();
+//		testCycle();
+	}
+	
+	private static void testCycle() {
 		LinkedList<String> list = new LinkedList<>();
 		list.add("first");
 		list.add("second");
 		list.add("third");
 		list.add("fourth");
 		list.add("fifth");
-		list.addAtHead("ten");
-		list.addAfterGivenElement("eleven", "fifth");
-		System.out.println("\nnumber of element in list is: " + list.size());
-		System.out.println("printing list once ready....");
+		list.traverse();
+		list.add("six");
+		list.add("seven");
+		list.add("eight");
+		list.add("nine");
+		list.add("ten");
+		list.formCycle();
+		System.out.println("\nCycle is detected in Given list: " + list.detectCycle());
+		
+		list.removeCycle();
+		System.out.println("\nCheck if list has still cycle: " + list.detectCycle());
 		list.traverse();
 		
-		list.deleteFirst();
+	}
+	
+	private static void testCircularity(){
+		LinkedList<String> list = new LinkedList<>();
+		list.addCircularly("first");
+		list.addCircularly("second");
+		list.addCircularly("third");
+		list.addCircularly("fourth");
+		list.addCircularly("fifth");
+		list.addCircularly("six");
+		list.addCircularly("seven");
+		list.addCircularly("eight");
+		list.addCircularly("nine");
+		list.addCircularly("ten");
+		
+		System.out.println("Given list is circular: " + list.isCircular());
+		list.removeCircularity();
+		list.traverse();
+		System.out.println("\nlenght of the list is " + list.findLingth());
+		
+		boolean search = list.search("nine");
+		System.out.println("nine is present in the list: " + search);
+		
+		/*LinkedList<String> list1 = new LinkedList<>();
+		list1.add("first");
+		list1.add("second");
+		list1.add("third");
+		list1.add("fourth");
+		list1.add("fifth");
+		list1.add("six");
+		list1.add("seven");
+		list1.add("eight");
+		list1.add("nine");
+		list1.add("ten");
+		
+		System.out.println("Given list is circular: " + list1.isCircular());*/
+	}
+	
+	
+	private static void retainMNodeThenDeleteMNodeTest(){
+		LinkedList<String> list = new LinkedList<>();
+		list.add("first");
+		list.add("second");
+		list.add("third");
+		list.add("fourth");
+		list.add("fifth");
+		list.add("six");
+		list.add("seven");
+		list.add("eight");
+		list.add("nine");
+		list.add("ten");
+		list.traverse();
+		System.out.println();
+		System.out.println(list.isCircular());
+//		LinkedList<String> linkedList = list.retainMDeleteN(2, 1);
+//		linkedList.traverse();
+		list.retainMDeleteN(1, 2);
+		list.traverse();
+	}
+	
+	
+	
+	
+
+	private static void testBasicFunctionOnLinkedList() {
+		LinkedList<String> list = new LinkedList<>();
+		list.add("first");
+		list.add("second");
+		list.add("third");
+		list.add("fourth");
+		list.add("fifth");
+//		list.addAtHead("ten");
+//		list.addAfterGivenElement("eleven", "fifth");
+		
+		System.out.println("Before reverse method called");
+		list.traverse();
+		
+		list.reverse();
+		
+		System.out.println("\nAfter reverse method called");
+		list.traverse();
+		
+		System.out.println("\nafter reversal add six at last and traverse the list");
+		
+		list.add("six");
+		list.traverse();
+		
+		
+		
+		/*System.out.println("\nnumber of element in list is: " + list.size());
+		System.out.println("printing list once ready....");
+		list.traverse();*/
+		
+		/*list.deleteFirst();
 		System.out.println("\nnumber of element in list after deleting from head: " + list.size());
 		System.out.println("after delete \n");
-		list.traverse();
+		list.traverse();*/
 		
 		/*list.deleteLast();
 		System.out.println("\nnumber of element in list after deleting last element: " + list.size());
--- LinkedList/src/main/java/com/learnjavabyexample/core/custom/linkedlist/LinkedList.java
+++ LinkedList/src/main/java/com/learnjavabyexample/core/custom/linkedlist/LinkedList.java
@@ -1,7 +1,7 @@
 package com.learnjavabyexample.core.custom.linkedlist;
 
 public class LinkedList<T> {
-	
+
 	private Node<T> head;
 	private Node<T> tail;
 	private int size;
@@ -17,7 +17,7 @@ public class LinkedList<T> {
 		public Node(T data) {
 			this.data = data;
 		}
-		
+
 		public T getData() {
 			return data;
 		}
@@ -33,40 +33,43 @@ public class LinkedList<T> {
 		public void setNext(Node<T> next) {
 			this.next = next;
 		}
+
+		@Override
+		public String toString() {
+			return this.data.toString();
+		}
 	}
-	
-	public void add(T data){
+
+	public void add(T data) {
 		Node<T> node = new Node<>(data);
-//		System.out.println("adding " + node);
-		if(head !=  null){
+		if (head != null) {
 			tail.setNext(node);
-		}else{
+		} else {
 			head = node;
 		}
 		tail = node;
 		size++;
 	}
-	
-	public void addAtHead(T data){
+
+	public void addAtHead(T data) {
 		Node<T> node = new Node<>(data);
-//		System.out.println("adding " + node);
-		if(head ==  null){
+		if (head == null) {
 			head = node;
-		}else{
+		} else {
 			node.setNext(head);
 			head = node;
 		}
 		size++;
 	}
-	
-	public void addAfterGivenElement(T data, T after){
-		if(head == null){
+
+	public void addAfterGivenElement(T data, T after) {
+		if (head == null) {
 			return;
 		}
 		Node<T> node = new Node<>(data);
 		Node<T> temp = head;
-		while(temp != null){
-			if(temp.getData().equals(after)){
+		while (temp != null) {
+			if (temp.getData().equals(after)) {
 				node.setNext(temp.getNext());
 				temp.setNext(node);
 				size++;
@@ -74,67 +77,217 @@ public class LinkedList<T> {
 			}
 			temp = temp.getNext();
 		}
-		if((temp != null) && (temp.getNext() == null)){
+		if ((temp != null) && (temp.getNext() == null)) {
 			tail = node;
 		}
 	}
-	
-	public void traverse(){
+
+	public void traverse() {
 		Node<T> temp = head;
-		while(temp != null){
-			System.out.println(temp.getData());
+		while (temp != null) {
+			if (temp.getNext() != null) {
+				System.out.print(temp.getData() + "-->");
+			} else {
+				System.out.print(temp.getData());
+			}
+
 			temp = temp.getNext();
 		}
 	}
 	
-	public void deleteFirst(){
-		if(head != null){
+	public boolean search(T data) {
+		Node<T> temp = head;
+		while (temp != null) {
+			if (temp.getData().equals(data)) {
+				return true;
+			} 
+			temp = temp.getNext();
+		}
+		return false;
+	}
+	
+	public int findLingth(){
+		if(this.head == null){
+			return 0;
+		}
+		int count = 0;
+		Node<T> temp = this.head;
+		while(temp != null){
+			count++;
+			temp = temp.getNext();
+		}
+		return count;
+	}
+
+	public void deleteFirst() {
+		if (head != null) {
 			Node<T> temp = head;
 			head = temp.getNext();
-			if(head.getNext() == null){
+			if (head.getNext() == null) {
 				tail = null;
 			}
-		}else{
+		} else {
 			return;
 		}
 		size--;
 	}
-	
-	public void deleteLast(){
+
+	public void deleteLast() {
 		Node<T> temp = head;
-		while(temp.getNext().getNext() != null){
+		while (temp.getNext().getNext() != null) {
 			temp = temp.getNext();
 		}
 		tail = temp;
 		temp.setNext(null);
 		size--;
 	}
-	
-	public void deleteWithGiveData(T data){
-		if(head == null){
+
+	public void deleteWithGiveData(T data) {
+		if (head == null) {
 			return;
 		}
 		Node<T> temp = head;
 		Node<T> prev = null;
-		while(temp != null){
-			if(temp.getData().equals(data)){
+		while (temp != null) {
+			if (temp.getData().equals(data)) {
 				size--;
 				break;
 			}
 			prev = temp;
 			temp = temp.getNext();
 		}
-		if(prev != null){
+		if (prev != null) {
 			prev.setNext(temp.getNext());
-		}else{
+		} else {
 			head = temp.getNext();
 		}
-		if(temp.getNext() == null){
+		if (temp.getNext() == null) {
 			tail = temp;
 		}
 	}
 
-	public int size(){
+	public void reverse() {
+		Node<T> prev = null;
+		Node<T> current = this.head;
+		Node<T> next = null;
+		while (current != null) {
+			if (prev == null) {
+				tail = current;
+			}
+			next = current.next;
+			current.next = prev;
+			prev = current;
+			current = next;
+		}
+		head = prev;
+	}
+
+	public void retainMDeleteN(int m, int n) {
+		Node<T> current = this.head;
+		Node<T> temp = null;
+
+		while (current != null) {
+			for (int count = 1; count < m && current != null; count++) {
+				current = current.getNext();
+			}
+			if (current != null) {
+				temp = current.getNext();
+			} else {
+				break;
+			}
+
+			for (int count = 1; count <= n && temp != null; count++) {
+				deleteWithGiveData(temp.getData());
+				temp = temp.getNext();
+			}
+			current.setNext(temp);
+			current = temp;
+		}
+		
+	}
+
+	public boolean isCircular(){
+		Node<T> temp = this.head.getNext();
+		if(head == null){
+			return true;
+		}
+		while ((temp != null) && (temp != head)) {
+			temp = temp.getNext();
+		}
+		return (temp == head);
+	}
+	
+	public void addCircularly(T data){
+		Node<T> node = new Node<>(data);
+		node.setNext(head);
+		if (head != null) {
+			tail.setNext(node);
+		} else {
+			head = node;
+		}
+		tail = node;
+		size++;
+	}
+	
+	public void removeCircularity(){
+		if(head == null){
+			return;
+		}
+		Node<T> first = head;
+		Node<T> second = head;
+		Node<T> prev = head;
+		
+		while((first != null) && (second != null) && (second.getNext() != null)){
+			prev = first;
+			first = first.getNext();
+			second = second.getNext().getNext();
+			if(first.equals(second)){
+				break;
+			}
+		}
+		prev.setNext(null);
+	}
+	
+	public void formCycle(){
+		tail.setNext(head.getNext());
+	}
+	
+	// Floyd’s Cycle finding algorithm.
+	public boolean detectCycle(){
+		if(head == null){
+			return true;
+		}
+		Node<T> first = head;
+		Node<T> second = head;
+		
+		while((first != null) && (second != null) && (second.getNext() != null)){
+			first = first.getNext();
+			second = second.getNext().getNext();
+			if(first.equals(second)){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public void removeCycle(){
+		if(head == null){
+			return;
+		}
+		Node<T> first = head;
+		Node<T> second = head;
+		
+		while((first != null) && (second != null) && (second.getNext() != null)){
+			first = first.getNext();
+			second = second.getNext().getNext();
+			if(first.equals(second)){
+				break;
+			}
+		}
+		first.setNext(null);
+	}
+	
+	public int size() {
 		return size;
 	}
 }
