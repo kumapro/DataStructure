8e545601c28e7faf2edcff62a4fd53c211dd48f5
Prem <premkmishra.info@gmail.com>
2017-04-06 17:28:26 +0530

    Merge sort implementation

---
+++ sorting/src/com/learnjavabyexample/ds/sorting/merge/Client.java
@@ -0,0 +1,26 @@
+package com.learnjavabyexample.ds.sorting.merge;
+
+public class Client {
+
+	public static void main(String[] args) {
+		int arr[] = {12, 11, 13, 5, 6, 7, 1};
+		 
+        System.out.println("Given Array");
+        printArray(arr);
+ 
+        MergeSort ob = new MergeSort();
+        ob.sort(arr, 0, arr.length-1);
+ 
+        System.out.println("\nSorted array");
+        printArray(arr);
+
+	}
+
+	private static void printArray(int[] arr) {
+		for (int i=0; i< arr.length ; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+		
+	}
+	
+}
